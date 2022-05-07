25889c3be077e6bf3091cf9e3f17fbcde7880f2a
Abdullah Javaid <abdullah.javaid0331@gmail.com>
2022-05-07 14:38:52 +0500

    add general button  and add constant

---
+++ lib/util/my_extensions.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:fyp/widget/text_widgets.dart';
+
+extension StringExtension on String {
+  MyText toText({
+    bool upperCase = false,
+    double? fontSize,
+    TextStyle? style,
+    TextAlign? textAlign,
+    FontWeight? fontWeight,
+    int? maxLines,
+    Color? color,
+    double letterSpacing = 0,
+    TextDecoration? decoration,
+    TextOverflow? overflow,
+  }) {
+    return MyText(
+      this,
+      fontSize: fontSize,
+      style: style,
+      upperCase: upperCase,
+      color: color,
+      textAlign: textAlign,
+      maxLines: maxLines,
+      fontWeight: fontWeight,
+      letterSpacing: letterSpacing,
+      decoration: decoration,
+      overflow: overflow,
+    );
+  }
+}
